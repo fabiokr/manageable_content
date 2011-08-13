dc91aa3c5af4ea04ef633442e1a9dd6d0cf38197
Fabio Kreusch <fabiokr@gmail.com>
2011-08-13 16:33:59 -0700

    Comented manageable_content_for

--- lib/manageable_content/controllers/dsl.rb
+++ lib/manageable_content/controllers/dsl.rb
@@ -9,6 +9,15 @@ module ManageableContent
 
       module ClassMethods
 
+        # Configures the manageable contents for a Controller.
+        # For example, if the Controller will have a 'body' and a 'side' contents,
+        # the following should be set:
+        #
+        #   manageable_content_for :body, :side
+        #
+        # This can also be called without parameters, in which case this will return the current 
+        # manageable contents for the Controller.
+        #
         def manageable_content_for(*keys)
           unless keys.empty?
             self.manageable_content_keys = keys
