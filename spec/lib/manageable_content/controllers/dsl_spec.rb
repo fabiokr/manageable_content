adc4bab1b99e394e65fea9ab20c4ea88759f0d52
Fabio Kreusch <fabiokr@gmail.com>
2011-08-13 18:28:28 -0700

    Refactored test

--- spec/lib/manageable_content/controllers/dsl_spec.rb
+++ spec/lib/manageable_content/controllers/dsl_spec.rb
@@ -20,35 +20,64 @@ describe "The Controller Dsl" do
   end
 
   context "instance methods" do
-    context "for a HomeController instance with configured pages" do
+    context "for controller instances with configured pages" do
       before :each do
-        @controller = HomeController.new
+        # HomeController
+        @home_controller = HomeController.new
 
-        @page        = ManageableContent::Page.new
-        @page.key    = @controller.controller_path
-        @page.locale = I18n.locale
-        @page.save!
+        @home_page        = ManageableContent::Page.new
+        @home_page.key    = @home_controller.controller_path
+        @home_page.locale = I18n.locale
+        @home_page.save!
 
-        @body_content         = @page.page_contents.build
-        @body_content.key     = :body
-        @body_content.content = "The body content"
-        @body_content.save!
+        @home_body_content         = @home_page.page_contents.build
+        @home_body_content.key     = "body"
+        @home_body_content.content = "The home body content"
+        @home_body_content.save!
 
-        @side_content         = @page.page_contents.build
-        @side_content.key     = :side
-        @side_content.content = "The side content"
-        @side_content.save!
+        @home_side_content         = @home_page.page_contents.build
+        @home_side_content.key     = "side"
+        @home_side_content.content = "The home side content"
+        @home_side_content.save!
+
+        # ContactController
+        @contact_controller = ContactController.new
+
+        @contact_page        = ManageableContent::Page.new
+        @contact_page.key    = @contact_controller.controller_path
+        @contact_page.locale = I18n.locale
+        @contact_page.save!
+
+        @contact_body_content         = @contact_page.page_contents.build
+        @contact_body_content.key     = "body"
+        @contact_body_content.content = "The contact body content"
+        @contact_body_content.save!
+
+        @contact_message_content         = @contact_page.page_contents.build
+        @contact_message_content.key     = "message"
+        @contact_message_content.content = "The side content"
+        @contact_message_content.save!
       end
 
       context "manageable_content_for helper" do
-        it "should retrieve the correct content for :body" do
-          @controller.manageable_content_for(:body).should == @body_content.content
+        context "HomeController" do
+          it "should retrieve the correct content for :body" do
+            @home_controller.manageable_content_for(:body).should == @home_body_content.content
+          end
+          it "should retrieve the correct content for :side" do
+            @home_controller.manageable_content_for(:side).should == @home_side_content.content
+          end
         end
-        it "should retrieve the correct content for :side" do
-          @controller.manageable_content_for(:side).should == @side_content.content
+
+        context "ContactController" do
+          it "should retrieve the correct content for :body" do
+            @contact_controller.manageable_content_for(:body).should == @contact_body_content.content
+          end
+          it "should retrieve the correct content for :message" do
+            @contact_controller.manageable_content_for(:message).should == @contact_message_content.content
+          end
         end
       end
     end
   end
-
 end
