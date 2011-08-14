dd305150f26982a247931df449a5386f9a9c79cd
Fabio Kreusch <fabiokr@gmail.com>
2011-08-13 17:19:24 -0700

    Refactored test

--- spec/lib/manageable_content/controllers/dsl_spec.rb
+++ spec/lib/manageable_content/controllers/dsl_spec.rb
@@ -2,18 +2,20 @@ require 'spec_helper'
 
 describe "The Controller Dsl" do
 
-  context "manageable_content_for method" do
-    it "should configure the content keys for the HomeController" do
-      HomeController.manageable_content_for.should == [:body, :side]
+  context "class methods" do
+    context "manageable_content_for" do
+      it "should configure the content keys for the HomeController" do
+        HomeController.manageable_content_for.should == [:body, :side]
+      end
+      it "should configure the content keys for the ContactController" do
+        ContactController.manageable_content_for.should == [:body, :message]
+      end
     end
-    it "should configure the content keys for the ContactController" do
-      ContactController.manageable_content_for.should == [:body, :message]
-    end
-  end
 
-  context "manageable_layout_content_for method" do
-    it "should configure the layout content keys for the ApplicationController" do
-      ApplicationController.manageable_layout_content_for.should == [:footer_copyright, :footer_contact]
+    context "manageable_layout_content_for" do
+      it "should configure the layout content keys for the ApplicationController" do
+        ApplicationController.manageable_layout_content_for.should == [:footer_copyright, :footer_contact]
+      end
     end
   end
 
