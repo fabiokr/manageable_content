7343ec5088a1f82983c9afea5abc538af2e5c152
Fabio Kreusch <fabiokr@gmail.com>
2011-08-17 09:28:58 -0700

    Moved general config to Engine initializer

--- config/initializers/default_configuration.rb
+++ config/initializers/default_configuration.rb
@@ -1,1 +1,23 @@
+# Configures the managable locales available for the application.
+# This is used while generating pages; So, if the application needs a page version
+# for the English and Portuguese locales, the following should be set in an initializer:
+#
+#   ManageableContent::Engine.config.locales = [:en, :pt]
+#
 ManageableContent::Engine.config.locales = [I18n.locale]
+
+# Configures the manageable contents that will be shared between all Controllers.
+# For example, if all Controllers will share a 'footer_message' and a 'footer_copyright' 
+# contents, the following should be set in an initializer:
+#
+#   ManageableContent::Engine.config.ignore_controller_namespaces :footer_message, :footer_copyright
+#
+ManageableContent::Engine.config.ignore_controller_namespaces = []
+
+# Configures default content keys for all Controllers.
+# For example, if all Controllers will have a 'title' and a 'keywords' contents,
+# the following should be set in an initializer:
+#
+#   ManageableContent::Engine.config.default_contents :title, :keywords
+#
+ManageableContent::Engine.config.default_contents = []
--- lib/manageable_content/controllers/dsl.rb
+++ lib/manageable_content/controllers/dsl.rb
@@ -3,11 +3,8 @@ module ManageableContent
     module Dsl
       extend ActiveSupport::Concern
         
-      mattr_accessor :manageable_ignore_controller_namespace_keys,
-                     :manageable_layout_content_keys, 
-                     :manageable_default_content_keys
-                     
-      self.manageable_layout_content_keys, self.manageable_default_content_keys = {}, []
+      mattr_accessor :manageable_layout_content_keys
+      self.manageable_layout_content_keys = {}
 
       included do
         class_attribute :manageable_content_keys
@@ -18,30 +15,6 @@ module ManageableContent
       end
 
       module ClassMethods
-
-        # Configures the manageable contents that will be shared between all Controllers.
-        # For example, if all Controllers will share a 'footer_message' and a 'footer_copyright' 
-        # contents, the following should be set on a high level Controller (e.g. ApplicationController):
-        #
-        #   manageable_layout_content_for :footer_message, :footer_copyright
-        #
-        def manageable_ignore_controller_namespaces(*keys)
-          unless keys.empty?
-            Dsl.manageable_ignore_controller_namespace_keys = keys.uniq
-          end
-        end
-
-        # Configures default content keys for all Controllers.
-        # For example, if all Controllers will have a 'title' and a 'keywords' contents,
-        # the following should be set:
-        #
-        #   manageable_default_content_for :title, :keywords
-        #
-        def manageable_default_content_for(*keys)
-          unless keys.empty?
-            Dsl.manageable_default_content_keys = keys.uniq
-          end
-        end
 
         # Configures the manageable contents that will be shared between all Controllers that
         # use a given layout.
